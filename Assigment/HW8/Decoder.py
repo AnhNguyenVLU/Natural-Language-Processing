b4530de7f3604f52826d069b1ba279caba60bacc
Nguyen Thai Anh <74288640+anhnguyen1008@users.noreply.github.com>
2023-03-13 01:13:33 +0700

    update

---
+++ Assigment/HW8/Decoder.py
@@ -0,0 +1,169 @@
+import torch.nn as nn
+import torch
+
+class MultiHeadAttentionLayer(nn.Module):
+    
+    def __init__(self, hid_dim, n_heads, dropout, device):
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.n_heads = n_heads
+        self.head_dim = hid_dim // n_heads  #make sure it's divisible....
+        
+        self.fc_q = nn.Linear(hid_dim, hid_dim)
+        self.fc_k = nn.Linear(hid_dim, hid_dim)
+        self.fc_v = nn.Linear(hid_dim, hid_dim)
+        self.fc   = nn.Linear(hid_dim, hid_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        self.scale   = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+        
+    def forward(self, q, k, v, mask = None):
+        b = q.shape[0]
+        
+        Q = self.fc_q(q)
+        K = self.fc_k(k)
+        V = self.fc_v(v)
+        #Q, K, V = [b, l, h]
+        
+        #reshape them into head_dim
+        #reshape them to [b, n_heads, l, head_dim]
+        Q = Q.view(b, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(b, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(b, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        #Q, K, V = [b, n_heads, l, head_dim]
+        
+        #e = QK/sqrt(dk)
+        e = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+        #e: [b, n_heads, ql, kl]
+        
+        if mask is not None:
+            e = e.masked_fill(mask == 0, -1e10)
+            
+        a = torch.softmax(e, dim=-1)
+        
+        #eV
+        x = torch.matmul(self.dropout(a), V)
+        #x: [b, n_heads, ql, head_dim]
+        
+        x = x.permute(0, 2, 1, 3).contiguous()
+        #x: [b, ql, n_heads, head_dim]
+        
+        #concat them together
+        x = x.view(b, -1, self.hid_dim)
+        #x: [b, ql, h]
+        
+        x = self.fc(x)
+        #x = [b, ql, h]
+        
+        return x, a
+class DecoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, device):
+        super().__init__()
+        
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm = nn.LayerNorm(hid_dim)
+        self.self_attention = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, trg, trg_mask):
+        
+        #trg = [batch size, trg len, hid dim]
+        #trg_mask = [batch size, 1, trg len, trg len]
+        
+        #self attention
+        _trg, _ = self.self_attention(trg, trg, trg, trg_mask)
+        
+        #dropout, residual connection and layer norm
+        trg = self.self_attn_layer_norm(trg + self.dropout(_trg))
+            
+        #trg = [batch size, trg len, hid dim]
+        
+        #positionwise feedforward
+        _trg = self.positionwise_feedforward(trg)
+        
+        #dropout, residual and layer norm
+        trg = self.ff_layer_norm(trg + self.dropout(_trg))
+        
+        #trg = [batch size, trg len, hid dim]
+        #attention = [batch size, n heads, trg len, src len]
+        
+        return trg
+class PositionwiseFeedforwardLayer(nn.Module):
+        
+    def __init__(self, hid_dim, pf_dim, dropout):
+        super().__init__()
+        self.fc1 = nn.Linear(hid_dim, pf_dim)
+        self.fc2 = nn.Linear(pf_dim, hid_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        return self.fc2(self.dropout(torch.relu(self.fc1(x))))   
+    
+class Decoder(nn.Module):
+    def __init__(self, output_dim, hid_dim, n_layers, n_heads, 
+                 pf_dim, dropout, trg_pad_idx, device, max_length = 100):
+        super().__init__()
+        
+        self.device = device
+        
+        self.trg_embedding = nn.Embedding(output_dim, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        
+        self.trg_pad_idx = trg_pad_idx
+        
+        self.layers = nn.ModuleList([DecoderLayer(hid_dim, 
+                                                  n_heads, 
+                                                  pf_dim, 
+                                                  dropout, 
+                                                  device)
+                                     for _ in range(n_layers)])
+        
+        self.fc_out = nn.Linear(hid_dim, output_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.scale = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
+    
+    def make_trg_mask(self, trg):
+        
+        #trg = [batch size, trg len]
+        
+        trg_pad_mask = (trg != self.trg_pad_idx).unsqueeze(1).unsqueeze(2)
+        #trg_pad_mask = [batch size, 1, 1, trg len]
+        
+        trg_len = trg.shape[1]
+        
+        trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device = self.device)).bool()
+        #trg_sub_mask = [trg len, trg len]
+            
+        trg_mask = trg_pad_mask & trg_sub_mask
+        #trg_mask = [batch size, 1, trg len, trg len]
+        
+        return trg_mask
+        
+    def forward(self, trg):
+        #trg = [batch size, trg len]
+        
+        trg_mask = self.make_trg_mask(trg)
+        #trg_mask = [batch size, 1, trg len, trg len]
+                
+        batch_size = trg.shape[0]
+        trg_len = trg.shape[1]
+        
+        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)          
+        #pos = [batch size, trg len]
+            
+        trg = self.dropout((self.trg_embedding(trg) * self.scale) + self.pos_embedding(pos))
+        #trg = [batch size, trg len, hid dim]
+                
+        for layer in self.layers:
+            trg = layer(trg, trg_mask)
+        
+        #trg = [batch size, trg len, hid dim]
+        #attention = [batch size, n heads, trg len, src len]
+        
+        output = self.fc_out(trg)
+        #output = [batch size, trg len, output dim]
+            
+        return output
